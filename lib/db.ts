288736289878f4bc6b04d3979fe34e19479bb3ea
Tanvir Raihan <raihncse43@gmail.com>
2026-04-15 11:02:31 +0600

    schema created

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,1 @@
-import "server-only";
-
-import "@/lib/env";
-import { PrismaClient } from "@prisma/client";
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+export { prisma } from "./prisma";
